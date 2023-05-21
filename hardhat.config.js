46387adab5d3922a54b721d075b1b6c84e001d1f
Vladimir Denisenko <101186417+AbuAdam07@users.noreply.github.com>
2023-05-21 16:02:35 +0300

    need url and private key

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,8 +3,8 @@ require("@nomiclabs/hardhat-waffle");
 module.exports = {
   networks: {
     goerli: {
-        url: "https://eth-goerli.g.alchemy.com/v2/SdVItf_NwJ7whx4g88ZXLn0O7ew9eqIW",
-        accounts: ["dbd5a099102c8f0fdf582568162aea3aec0e2dc8f2bc6e41ff0209be919ae24f"]
+        url: "URL HTTP,
+        accounts: ["PRIVATE KEY"]
     }
   },
   solidity: "0.8.18",
